17a03b05bf40eae0b6107d04deb8e66280f600c3
flame <yaolynzc@126.com>
2017-08-29 10:31:23 +0800

    调整为批量插入mysql

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var path = require('path');
 var bencode = require('bencode');
 var P2PSpider = require('./lib');
 var torrentParser = require('torrent-parser');
+var torlistsarr = [];
 
 // 1.引入mysql模块
 var mysql = require('mysql');
@@ -43,26 +44,36 @@ p2p.on('metadata', function (metadata) {
             // console.log("Name:" + parsedTorrent.name);
             
             var filelist = JSON.stringify(parsedTorrent.files);
-            // 常见几种视频文件类型的检测
+            // 常见几种文件类型的检测
             var videoRegEx = new RegExp("^.+\.(mkv)|(mp4)|(avi)|(rmvb)|(wmv)|(rm)|(mpeg)|(ts)$");
             
-            // 如果包含以上8种视频文件，则保存
+            // 如果包含以上8种文件类型，则保存
             if (videoRegEx.test(filelist.toLowerCase())) {
-                // 定义mysql数据对象
-                var torlists = {
-                    ID: parsedTorrent.infoHash,
-                    NAME: parsedTorrent.name,
-                    FILES: JSON.stringify(filelist)
-                };
+                // // 定义mysql数据对象
+                // var torlists = {
+                //     ID: parsedTorrent.infoHash,
+                //     NAME: parsedTorrent.name,
+                //     FILES: filelist
+                // };
 
-                var query = connection.query('INSERT INTO torlists SET ?', torlists, function (error, results, fields) {
-                    if (error) throw error;
-                    // console.log(fields);
-                    
-                    // 删除非以上8种视频文件的种子
-                    fs.unlink(torrentFilePathSaveTo,function(error){
-                        if(error) throw error;
+                // var query = connection.query('INSERT INTO torlists SET ?', torlists, function (error, results, fields) {
+                //     if (error) throw error;
+                // // console.log(fields);
+                // });
+
+                var torlists = [parsedTorrent.infoHash,parsedTorrent.name,filelist];
+                torlistsarr.push(torlists);
+                // 匹配到300个文件时才执行批量保存到后台mysql数据库操作
+                if(torlistsarr.length == 300){
+                    var query = connection.query('INSERT INTO torlists(ID,NAME,FILES) VALUES ?', [torlistsarr], function (error, rows, fields) {
+                        if (error) throw error;
+                        torlistsarr = [];
                     });
+                }
+            }else{
+                // 删除非以上8种类型的文件
+                fs.unlink(torrentFilePathSaveTo,function(error){
+                    if(error) throw error;
                 });
             }
         }
